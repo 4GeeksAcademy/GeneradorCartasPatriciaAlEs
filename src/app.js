8a68a225c4a5ff1403ab2339c5fa98884cc95f46
PatriciaAlEs <168550797+PatriciaAlEs@users.noreply.github.com>
2024-07-22 18:25:41 +0000

    Falta boton

--- src/app.js
+++ src/app.js
@@ -1,11 +1,48 @@
-/* eslint-disable */
-import "bootstrap";
+/* eslint-disable no-console */
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+const numeroDeCartas = ["A", "1", "2", "3", "4", "5", "6", "7", "J", "Q", "K"];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  const resultadoNumeroRandom = devuelveNumeroRandom();
+  const resultadoPaloRandom = devuelvePaloRandom();
+
+  // Asignar los resultados al DOM (por ID)
+  document.getElementById("numero").innerHTML = resultadoNumeroRandom;
+  document.getElementById("palo-arriba").innerHTML = resultadoPaloRandom;
+  document.getElementById("palo-abajo").innerHTML = resultadoPaloRandom;
+
+  aplicarColorPalo(resultadoPaloRandom);
 };
+function devuelveNumeroRandom() {
+  const numeroIndex = Math.floor(Math.random() * numeroDeCartas.length);
+  return numeroDeCartas[numeroIndex];
+}
+
+function devuelvePaloRandom() {
+  const palosDeCartas = {
+    rombos: "♦",
+    corazones: "♥",
+    picas: "♠",
+    treboles: "♣"
+  };
+  // Obtener las claves(key) del objeto en un array
+  const clavesDelObjetoPalos = Object.keys(palosDeCartas);
+  // Seleccionar una clave(key) aleatoria del array
+  const claveAleatoriaPalos =
+    clavesDelObjetoPalos[
+      Math.floor(Math.random() * clavesDelObjetoPalos.length)
+    ];
+  // Devuelve el valor correspondiente a la clave(key) aleatoria
+  return palosDeCartas[claveAleatoriaPalos];
+}
+
+function aplicarColorPalo(palo) {
+  const paloArribaElemento = document.getElementById("palo-arriba");
+  const paloAbajoElemento = document.getElementById("palo-abajo");
+
+  if (palo === "♥" || palo === "♠") {
+    paloAbajoElemento.style.color = "red";
+    paloArribaElemento.style.color = "red";
+  }
+}
